d00a5faa5a18205c645b6867065e0ca222ce3faf
shalini Kumarii <bittukumari@gmail.com>
2022-06-17 23:40:01 -0700

    Updated

--- Main/tictactoe.js
+++ Main/tictactoe.js
@@ -1,3 +1,13 @@
+/*Tic-tac-toe is played by two players A and B on a 3 x 3 grid. The rules of Tic-Tac-Toe are:
+
+Players take turns placing characters into empty squares ' '.
+The first player A always places 'X' characters, while the second player B always places 'O' characters.
+'X' and 'O' characters are always placed into empty squares, never on filled ones.
+The game ends when there are three of the same (non-empty) character filling any row, column, or diagonal.
+The game also ends if all squares are non-empty.
+No more moves can be played if the game is over.*/
+
+
 const moves1=[[0,0],[1,1],[2,0],[1,0],[1,2],[2,1],[0,1],[0,2],[2,2]];
 const moves2 = [[0,0],[1,1],[0,1],[0,2],[1,0],[2,0]]
 function tictactoe(moves)
@@ -20,6 +30,7 @@ function tictactoe(moves)
 
         rows[row]+=player;
         cols[col]+=player;
+        console.log("player="+player);
 
         if(row==col)
         {
@@ -40,8 +51,8 @@ function tictactoe(moves)
                     return "B";
                 }
             }
-
-            player *=-1;
+            
+            player =-1;
     }
 
     if(moves.length == n*n)
